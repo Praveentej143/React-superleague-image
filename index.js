0a8f20589be770daab1748c96a3e96ff8c65c59d
Your Name <you@example.com>
2022-11-02 08:37:40 +0000

    basics of react

---
+++ index.js
@@ -0,0 +1,22 @@
+const element = (
+  <div className="bgContainer">
+    <div className="card-container">
+      <div className="card">
+        <h1 className="heading">Super Over League</h1>
+      </div>
+      <div className="img-container">
+        <img
+          className="img"
+          src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        />
+        <img
+          className="img"
+          src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        />
+      </div>
+    </div>
+  </div>
+  //   Write your code here.
+);
+
+ReactDOM.render(element, document.getElementById("root"));
